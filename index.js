48d9a6cabca83e24cacf3e83f0d2b655581220f2
Mít <59923012+fakemythdora@users.noreply.github.com>
2022-01-09 11:17:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Bot của bạn đã đã online!!!'))
+app.get('/', (req, res) => res.send('Hello World!!!'))
 
 app.listen(port, () =>
-console.log(`Đang dùng web http://localhost:${port} để host! Good Luck :D`) //Just a cap from my brother ;-; Please dont delete -> ${port}
+console.log(`Đang dùng app http://localhost:${port}!`)
 );
